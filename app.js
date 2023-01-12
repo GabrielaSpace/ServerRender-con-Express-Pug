f3a6c81d21fd67df9d810109745845a49871ebf5
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-01-12 17:35:21 +0100

    mitad css

--- app.js
+++ app.js
@@ -4,50 +4,28 @@ const fetch = require('node-fetch');
 const port = 3000;
 const app = express();
 app.use(express.json()); 
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Template engine
 app.use(express.static('public'));
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/film", (req, res) => {
-  res.render("film");
-});
-
-
-/* app.post('/film', (req, res) => {
-  const data= JSON.parse(body.title);
-  console.log(data)
-  console.log("*******DATOS ENVIADOS*******");
-  console.log(req.body);
-  const {title} = req.body;
-  res.status(201).json({msj:`Movie: ${title}`});
-})
-
-
-
-//Title,Genre,
 app.get('/film/:title', async (req, res) => {
-  if (Madrid) {
-      try {
-          let response = await fetch(`http://www.omdbapi.com/?t=Madrid&apikey=bd13e4ce`);
-          let film = await response.json(); //{}
-          res.render('film'); // Pinta datos en el pug
-      }
-      catch (error) {
-          console.log(`ERROR: ${error.stack}`);
-      }
-    } else{
-      console.log('error')
-    } 
-});
+  if (req.params.title) {
+          let resp = await fetch(`http://www.omdbapi.com/?t=${req.params.title}&apikey=bd13e4ce`);
+          let film = await resp.json(); 
+          console.log(film.Title)
+          console.log(film.Genre)
+          res.render("film",{film}); // Pinta datos en el pug
+    }});
+app.post('/', (req, res) =>
+res.redirect(`/film/${req.body.title}`))
 
 
- */
-
-app.listen(port, () => console.log(`Serving on ${port} https://localhost:3000`));
+app.listen(port, () => console.log(`Serving on ${port} http://localhost:3000`));
 
